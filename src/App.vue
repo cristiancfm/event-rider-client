1727b5ffa9d88326b831fa91cb3d08b7448dc84e
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2023-06-10 21:19:34 +0200

    Added app footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <AppNavbar></AppNavbar>
-  <div style="max-width: 1280px; margin: auto">
+  <div style="max-width: 1280px; min-height: calc(100vh - 92px); margin: auto">
     <!--  With the key, any change to the path will trigger a reload of the component,
    event if it is the same component being loaded -->
     <router-view :key="$route.fullPath" />
   </div>
+  <AppFooter></AppFooter>
 </template>
 
 <script>
 import AppNavbar from "@/components/AppNavbar";
+import AppFooter from "@/components/AppFooter";
 
 export default {
-  components: { AppNavbar },
+  components: { AppFooter, AppNavbar },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="navbar bg-light justify-content-center">
+    <div>
+      <p class="m-0">Event Rider. Licensed under MIT.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
+
+<style scoped></style>
